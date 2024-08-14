0bc8e63b0e0319b97f09ed8e1922d53d90238518
v3nkat3shk <venakteshkumbhar5700@gmail.com>
2024-08-14 14:20:48 +0530

    added new colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,15 @@
 return {
-  {
-    "loctvl842/monokai-pro.nvim",
-    config = function()
-      require("monokai-pro").setup()
-    end
-  },
-  {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("solarized-osaka").setup({
-        transparent = false,
-      })
-    end
-  },
+	{
+		"craftzdog/solarized-osaka.nvim",
+		config = function()
+			require("solarized-osaka").setup({
+				transparent = false,
+			})
+		end,
+	},
+	{
+		"oxfist/night-owl.nvim",
+		lazy = false,
+		priority = 1000,
+	},
 }
