8f1bc0cef3f9532a276c1de127628ff4a1c3ca8d
v3nkat3shk <venakteshkumbhar5700@gmail.com>
2024-08-14 14:20:13 +0530

    refactored code for lsp setup

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,54 +1,53 @@
 local k = vim.keycode
-vim.g.mapleader = k('<space>')
+vim.g.mapleader = k("<space>")
 vim.g.termguicolors = true
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-  install = { colorscheme = { "habamax" } },
-  checker = {
-    enabled = true,
-    notify = false,
-  }, -- automactically check for plugin updates
-  performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
-
+	spec = {
+		{ import = "plugins" },
+	},
+	-- install = { colorscheme = { "habamax" } },
+	checker = {
+		enabled = true,
+		notify = false,
+	}, -- automactically check for plugin updates
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip",
+				-- "matchit",
+				-- "matchparen",
+				-- "netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
 
 local modules = { "config.autocmds", "config.options", "config.keymaps", "config.custom" }
 
 for _, mod in ipairs(modules) do
-  pcall(require, mod)
+	pcall(require, mod)
 end
 
-vim.cmd('colorscheme solarized-osaka-storm')
+vim.cmd.colorscheme("night-owl")
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,76 +1,36 @@
 return {
-  -- lsp config
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "hrsh7th/cmp-nvim-lsp" },
-    lazy = true,
-    config = function()
-      local nvim_lsp = require("lspconfig")
-      local mason_lspconfig = require("mason-lspconfig")
-      -- local protocol = require("vim.lsp.protocol")
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		-- "WhoIsSethDaniel/mason-tool-installer.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"stevearc/conform.nvim",
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	lazy = true,
+	config = function()
+		-- local protocol = require("vim.lsp.protocol")
+		require("mason").setup()
+		require("mason-lspconfig").setup({ automatic_installation = { exclude = { "pylsp" } } })
+		-- auto formating
+		require("conform").setup({
+			formatters_by_ft = { lua = { "stylua" }, python = { "isort", "black" } },
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		})
 
-      local on_attach = function(client, bufnr)
-        if client.server_capabilities.documentFormattingProvider then
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = vim.api.nvim_create_augroup("Format", { clear = true }),
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format()
-            end,
-          })
-        end
-      end
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local nvim_lsp = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      mason_lspconfig.setup_handlers({
-        function(server)
-          nvim_lsp[server].setup({
-            capabilities = capabilities,
-          })
-        end,
-        ["pylsp"] = function()
-          nvim_lsp["pylsp"].setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end,
-        ["lua_ls"] = function()
-          nvim_lsp["lua_ls"].setup({
-            on_init = function(client)
-              local path = client.workspace_folders[1].name
-              if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-                return
-              end
+		nvim_lsp["pylsp"].setup({
+			capabilities = capabilities,
+		})
 
-              client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                runtime = {
-                  -- Tell the language server which version of Lua you're using
-                  -- (most likely LuaJIT in the case of Neovim)
-                  version = 'LuaJIT'
-                },
-                -- Make the server aware of Neovim runtime files
-                workspace = {
-                  checkThirdParty = false,
-                  library = {
-                    vim.env.VIMRUNTIME,
-                    -- Depending on the usage, you might want to add additional paths here.
-                    "${3rd}/luv/library"
-                    -- "${3rd}/busted/library",
-                  }
-                  -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                  -- library = vim.api.nvim_get_runtime_file("", true)
-                }
-              })
-            end,
-            settings = {
-              Lua = {}
-            },
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end
-      })
-    end
-  },
+		nvim_lsp["lua_ls"].setup({
+			capabilities = capabilities,
+		})
+	end,
 }
