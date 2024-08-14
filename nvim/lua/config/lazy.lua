d57b3c7b7499e7baa659c8a6920aa0ff0cf2bb0f
v3nkat3shk <venakteshkumbhar5700@gmail.com>
2024-08-14 18:17:18 +0530

    added nord theme and update setup for lualing

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -50,4 +50,4 @@ for _, mod in ipairs(modules) do
 	pcall(require, mod)
 end
 
-vim.cmd.colorscheme("night-owl")
+vim.cmd.colorscheme("nord")
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,24 @@
 return {
 	{
 		"craftzdog/solarized-osaka.nvim",
-		config = function()
-			require("solarized-osaka").setup({
-				transparent = false,
-			})
-		end,
+		opts = {
+			transparent = false,
+		},
 	},
 	{
 		"oxfist/night-owl.nvim",
+	},
+	{
+		"shaunsingh/nord.nvim",
 		lazy = false,
 		priority = 1000,
+		config = function()
+			vim.g.nord_contrast = true
+			vim.g.nord_borders = true
+			vim.g.nord_disable_background = false
+			vim.g.nord_italic = true
+			vim.g.nord_uniform_diff_background = false
+			vim.g.nord_bold = false
+		end,
 	},
 }
