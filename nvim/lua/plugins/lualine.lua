c73ab46b3be1b59d08b52d6fd5e787650329b895
v3nkat3shk <venakteshkumbhar5700@gmail.com>
2024-08-16 10:26:05 +0530

    changed lualine config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -11,10 +11,10 @@ return {
 			},
 			sections = {
 				lualine_a = { "mode" },
-				lualine_b = {
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = {
 					{ "filename", file_status = false, path = 4 },
 				},
-				lualine_c = { "branch", "diff", "diagnostics" },
 				lualine_x = { "encoding", "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
