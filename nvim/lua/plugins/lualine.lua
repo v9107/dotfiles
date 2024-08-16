c22065ed7ef5941b5d3b8e3e0bea224619b3a60f
v3nkat3shk <venakteshkumbhar5700@gmail.com>
2024-08-16 10:17:44 +0530

    not satisfied by the color schemes

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,11 +1,24 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = {
-			theme = "iceberg_dark",
-			section_separators = "",
-			component_separators = "",
-		},
-	},
+	-- dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = false,
+				theme = "iceberg_dark",
+				section_separators = "",
+				component_separators = "",
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {
+					{ "filename", file_status = false, path = 4 },
+				},
+				lualine_c = { "branch", "diff", "diagnostics" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
